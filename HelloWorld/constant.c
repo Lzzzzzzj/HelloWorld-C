85994190fe564802a59960425d3bf8d9dbc3f5ff
LLL <2213577410@qq.com>
2024-03-18 15:51:09 +0800

    ex1

--- HelloWorld/constant.c
+++ HelloWorld/constant.c
@@ -1,26 +1,26 @@
-#include <stdio.h>
-int main() {
-
-	printf("d:%d", 19);
-	printf("f:%f", 19.77);
-	printf("s:%s", "hello");
-	printf("c:%c\r\n", 'c');
-
-	//我亲亲女朋友的姓名是：小诗诗。性别：女。年龄：18岁。身高：1米82。体重：110斤。
-	printf("我亲亲女朋友的姓名是：%s\n。性别：%s\n。年龄：%d岁\n。身高：%f\n。体重：%d斤。", "aaa", "a", 12, 1.4, 11);
-
-
-	int a;
-	a = 10;
-	printf("%d\r\n", a);
-
-	int b = 10;
-
-	b = b + b;
-
-	printf("%d", a + b);
-
-
-
-	return 0;
-}
+//#include <stdio.h>
+//int main() {
+//
+//	printf("d:%d", 19);
+//	printf("f:%f", 19.77);
+//	printf("s:%s", "hello");
+//	printf("c:%c\r\n", 'c');
+//
+//	//我亲亲女朋友的姓名是：小诗诗。性别：女。年龄：18岁。身高：1米82。体重：110斤。
+//	printf("我亲亲女朋友的姓名是：%s\n。性别：%s\n。年龄：%d岁\n。身高：%f\n。体重：%d斤。", "aaa", "a", 12, 1.4, 11);
+//
+//
+//	int a;
+//	a = 10;
+//	printf("%d\r\n", a);
+//
+//	int b = 10;
+//
+//	b = b + b;
+//
+//	printf("%d", a + b);
+//
+//
+//
+//	return 0;
+//}
---
+++ HelloWorld/ex1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main() {
+
+	int blood = 100;
+	
+	printf("初始血量：%d\r\n", blood);
+	blood = blood - 80;
+	printf("被攻击，血量：%d\r\n", blood);
+
+	blood = blood + 60;
+	printf("被治疗，血量：%d\r\n", blood);
+	printf("最终血量：%d\r\n", blood);
+
+
+
+	return 0;
+}
