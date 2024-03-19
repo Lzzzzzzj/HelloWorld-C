fd3672b47a50444428ddfa319c5a654d4a4e99fb
LLL <2213577410@qq.com>
2024-03-19 21:08:27 +0800

    var_test3

---
+++ HelloWorld/var_test3.c
@@ -0,0 +1,34 @@
+#include<stdio.h>
+
+int main(){
+
+	// 打印 short int long longlong
+
+	short a = 100;
+	printf("%d\n", a);
+
+	int b = 100;
+	printf("%d\n", b);
+
+	long c = 122;
+	printf("%ld\n", c);
+
+	long long d = 12334;
+	printf("%lld\n",d);
+
+	// 打印 各种类型 占用的字节大小
+
+	printf("%d\n", sizeof(short));
+	printf("%d\n", sizeof(a));
+
+	printf("%d\n", sizeof(int));
+	printf("%d\n", sizeof(b));
+
+	printf("%ld\n", sizeof(long));
+	printf("%d\n", sizeof(c));
+
+	printf("%lld\n", sizeof(long long));
+	printf("%d\n", sizeof(d));
+
+	return 0;
+}
